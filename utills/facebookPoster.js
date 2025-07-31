77be1b66aeb44d69c527cf0f2f016a83289dcdbc
Mohammad Atikuzzaman <akash203037@gmail.com>
2025-07-31 20:54:54 +0600

    facebook post using graph api is added

--- utills/facebookPoster.js
+++ utills/facebookPoster.js
@@ -1,30 +1,25 @@
-import fs from "fs";
-import path from "path";
 import axios from "axios";
-import FormData from "form-data";
-import { config } from "../config/aiConfig.js";
+import dotenv from "dotenv";
+dotenv.config();
 
+const fb_access_token =
+  "";
+const fb_page_id = "";
 
-export async function 
-postToFacebook(articleText, imagePath) {
-  try {
-    const form = new FormData();
-    form.append("caption", articleText);
-    form.append("access_token", config.fbAccessToken);
-    form.append("source", fs.createReadStream(path.resolve(imagePath)));
+export async function createPublicPost(message) {
+  const endpoint = `https://graph.facebook.com/v23.0/${fb_page_id}/photos?access_token=${fb_access_token}`;
 
-    const url = `https://graph.facebook.com/${config.fbPageId}/photos`;
+  const postData = {
+    message,
+    url: "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQjaZqoKF8yK88wv0RQrglNCHojWUuSZQt17pddpwQVVyjIqZfonSl85849PK3Es0KLxpBNx11PACke7SujhbDJTA",
+  };
 
-    const response = await axios.post(url, form, {
-      headers: {
-        ...form.getHeaders()
-      }
-    });
-
-    console.log("Posted to Facebook:", response.data);
-    return response.data;
-  } catch (error) {
-    console.error("Failed to post on Facebook:", error.response?.data || error.message);
-    throw error;
-  }
+  const response = await axios.post(endpoint, postData, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  console.log(response.data);
 }
+
+createPublicPost("Hello from node js, this is 31 jul 2025");
